646e40cd43f53b8bfba0764bb2be98c75c543a99
Brian Hartin <brian.hartin@banno.com>
2015-03-18 22:27:55 -0500

    Adding test which helps explain the CanTruthy (Yes-No) example

--- src/test/scala/CanTruthySpec.scala
+++ src/test/scala/CanTruthySpec.scala
@@ -1,59 +1,91 @@
 package com.banno.scalaz
 
-//import org.specs2.specification.Scope
 import org.specs2.mutable.Specification
-//import scalaz._
-//import Scalaz._
 
 class CanTruthySpec extends Specification {
-  "Given a parameterized type CanTruthy[A] " >> {
 
-    trait CanTruthy[A] { self =>
-      def truthys(a: A): Boolean
-    }
+  "Given the CanTruthy example code" >> {
 
-    "Can define CanTruthys for a type (Int)" >> {
+    import CanTruthyExample._
+
+    "Can define an instance of CanTruthy" >> {
+
       val intTruthys = new CanTruthy[Int] {
         def truthys(i: Int): Boolean = i >= 0
       }
 
-      intTruthys.truthys(1) must_== false
-      intTruthys.truthys(-1) must_== false
+      intTruthys.truthys(1) must_== true
+    }
 
-      "Can define a companion object" >> {
-        object CanTruthy {
-          def apply[A](implicit ev: CanTruthy[A]): CanTruthy[A] = ev
-          def truthys[A](f: A => Boolean): CanTruthy[A] = new CanTruthy[A] {
-            def truthys(a: A): Boolean = f(a)
-          }
-        }
+    "CanTruthy[A]() returns any implicitly available instance for A" >> {
 
-        1 must_== 1
+      implicit val intTruthys = new CanTruthy[Int] {
+        def truthys(i: Int): Boolean = i >= 0
       }
 
+      CanTruthy[Int] must_== intTruthys
+    }
+
+    "CanTruthys.truthys[A]() creates an instance using an anonymous function" >> {
+
+      val intTruthys = CanTruthy.truthys[Int](_ >= 0)
+
+      intTruthys.truthys(1) must_== true
+    }
+
+    "Can 'pimp' type A via implicit conversion to CanTruthyOps[A] " >> {
+
+      implicit val intTruthys = CanTruthy.truthys[Int](_ >= 0)
+      import ToCanIsTruthyOps._
+
+      1.truthy must_== true
+      -1.truthy must_== false
+
+      /*
+
+       Let's break down how this works.
+
+       - We've imported the examples, of course.
+       - We've defined an implicit instance of CanTruthy[Int].
+          - This defines what truthy means for Int.
+          - We've used the factory method on the companion object to do this succinctly
+
+       - By calling 1.truthy, Scala finds no 'truthy' on Int or its ancestors
+         so it looks for an implicit conversion.
+         - We've imported one from ToCanIsTruthyOps.
+           - This, in turn, needs an implicit parameter of type CanTruthy[Int]
+             - We got that, as described above
+           - Now it can create a new CanTruthyOps, using the implicit parameter.
+             This has the 'truthy' implementation, which delegates to CanTruthy[Int].
+      */
     }
   }
 
-/*
+  object CanTruthyExample {
+    trait CanTruthy[A] { self =>
+      def truthys(a: A): Boolean
+    }
 
+    object CanTruthy {
+      def apply[A](implicit ev: CanTruthy[A]): CanTruthy[A] = ev
+      def truthys[A](f: A => Boolean): CanTruthy[A] = new CanTruthy[A] {
+        def truthys(a: A): Boolean = f(a)
+      }
+    }
 
+    trait CanTruthyOps[A] {
+      def self: A
+      implicit def F: CanTruthy[A]
+      final def truthy: Boolean = F.truthys(self)
+    }
 
-
-
-
-
-  /**
-    The companion object for CanTruthy gives us:
-
-    - an apply for a type A which expects an implicit instance of
-    CanTruthy[A] to be in scope and, finding it, returns it.
-    - a factory method to create an instance of CanTruthy[A] given
-    a function to use as its truthys() implementation.
-    */
-  /*
-
-   */
-
-
-   */
+    object ToCanIsTruthyOps {
+      // Added return type for clarity.
+      implicit def toCanIsTruthyOps[A](v: A)(implicit ev: CanTruthy[A]): CanTruthyOps[A] =
+        new CanTruthyOps[A] {
+          def self = v
+          implicit def F: CanTruthy[A] = ev
+        }
+    }
+  }
 }
